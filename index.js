86554b94cd7a8df95e3674faaf3d4957f64a6605
Cleywert Rayol <cleywertrayol3@gmail.com>
2022-01-09 01:23:12 -0400

    fix: Inicialização do projeto

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const connection = require('./config/database');
+
+// conectando ao banco
+connection.authenticate().then(() => {
+    console.log("Conectado ao banco");
+}).catch(err => {
+    console.log(err);
+});
+
+// habilitando o CORS
+app.use(cors());
+
+// setando o body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// definindo pasta de midias estáticas
+app.use(express.static("public"));
+
+// importando controllers
+// os models de cada controller é importado dentro do mesmo e será executado
+const ControllerUser = require("./controllers/controllerUser");
+app.use("/", ControllerUser)
+
+// Setando porta da aplicação
+app.listen(8000, () => {
+    console.log("Servidor rodando");
+})
